a134d801ba98d6e30cbdeb148667ac4f0c64140e
Kyle <kyle@cooperkyle.com>
2024-07-20 18:09:20 -0400

    rendering triangles

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const gl = @cImport({
     @cInclude("glfw3.h");
     @cInclude("gl3.h");
 });
+const shaders = @import("shaders.zig");
 
 const OpenglInfo = struct {
     majorVersion: c_int,
@@ -13,7 +14,7 @@ const OpenglInfo = struct {
     shadingLangVer: [*:0]const u8,
 };
 
-fn getOpenglInfo() OpenglInfo {
+fn getCurrentOpenGlInfo() OpenglInfo {
     const vendor = gl.glGetString(gl.GL_VENDOR);
     const renderer = gl.glGetString(gl.GL_RENDERER);
     const version = gl.glGetString(gl.GL_VERSION);
@@ -48,6 +49,23 @@ fn keyCallback(
     }
 }
 
+fn frameBufferSizeCallback(
+    win: ?*gl.GLFWwindow,
+    width: c_int,
+    height: c_int,
+) callconv(.C) void {
+    _ = width;
+    _ = height;
+    _ = win;
+}
+
+fn openglCheckError() void {
+    const err = gl.glGetError();
+    if (err != gl.GL_NO_ERROR) {
+        std.debug.print("[ERROR] Opengl Error: {d}\n", .{err});
+    }
+}
+
 pub fn main() !void {
     std.debug.print("Launching 'game time!'\n", .{});
     if (gl.glfwInit() == gl.GL_FALSE) {
@@ -57,35 +75,79 @@ pub fn main() !void {
     gl.glfwWindowHint(gl.GLFW_CONTEXT_VERSION_MAJOR, 4);
     gl.glfwWindowHint(gl.GLFW_CONTEXT_VERSION_MINOR, 6);
     gl.glfwWindowHint(gl.GLFW_OPENGL_FORWARD_COMPAT, gl.GL_TRUE);
-    //.glfwWindowHint(gl.GLFW_OPENGL_DEBUG_CONTEXT, gl.is_on);
+    gl.glfwWindowHint(gl.GLFW_OPENGL_DEBUG_CONTEXT, gl.GL_TRUE); // TODO we dont want this for release
     gl.glfwWindowHint(gl.GLFW_OPENGL_PROFILE, gl.GLFW_OPENGL_CORE_PROFILE);
     gl.glfwWindowHint(gl.GLFW_DEPTH_BITS, 0);
     gl.glfwWindowHint(gl.GLFW_STENCIL_BITS, 8);
-    gl.glfwWindowHint(gl.GLFW_RESIZABLE, gl.GL_FALSE);
+    gl.glfwWindowHint(gl.GLFW_RESIZABLE, gl.GL_TRUE);
 
     const window = gl.glfwCreateWindow(800, 800, "Game Time", null, null) orelse @panic("Could not create glfw window");
     defer gl.glfwDestroyWindow(window);
     gl.glfwMakeContextCurrent(window);
 
+    // opengl callbacks
     _ = gl.glfwSetKeyCallback(window, keyCallback);
     _ = gl.glfwSetErrorCallback(errorCallback);
+    _ = gl.glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
 
-    const openglInfo = getOpenglInfo();
+    const openglInfo = getCurrentOpenGlInfo();
     std.debug.print("openglInfo'\n", .{});
     std.debug.print("major {}'\n", .{openglInfo.majorVersion});
     std.debug.print("minor {}'\n", .{openglInfo.minorVersion});
 
-    gl.glClearColor(1.0, 0.0, 0.0, 1.0);
+    gl.glEnable(gl.GL_DEPTH_TEST);
     gl.glViewport(0, 0, 800, 800);
     gl.glfwSwapInterval(1);
-    //const startTime = gl.glfwGetTime();
 
+    // build shaders
+    const baseVs = try shaders.createShader(shaders.vs, gl.GL_VERTEX_SHADER);
+    const baseFs = try shaders.createShader(shaders.fs, gl.GL_FRAGMENT_SHADER);
+    const programId = gl.glCreateProgram();
+    gl.glAttachShader(programId, baseVs);
+    gl.glAttachShader(programId, baseFs);
+    gl.glLinkProgram(programId);
+    if (shaders.checkCompileLinkError(programId)) |_| {} else |err| switch (err) {
+        else => std.debug.print("[ERROR] Failed to link program with id {d} due to error {}'\n", .{ programId, err }),
+    }
+    // build buffers
+    var vao: gl.GLuint = undefined;
+    var vbo: gl.GLuint = undefined;
+    gl.glGenVertexArrays(1, &vao);
+    gl.glGenBuffers(1, &vbo);
+    gl.glBindVertexArray(vao);
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo);
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, vertices.len * @sizeOf(gl.GLfloat), &vertices, gl.GL_STATIC_DRAW);
+
+    // pos cords
+    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 5 * @sizeOf(gl.GLfloat), null);
+    gl.glEnableVertexAttribArray(0);
+    // texture coord attribute
+    gl.glVertexAttribPointer(1, 2, gl.GL_FLOAT, gl.GL_FALSE, 5 * @sizeOf(gl.GLfloat), @ptrFromInt((3 * @sizeOf(gl.GLfloat))));
+    gl.glEnableVertexAttribArray(1);
+    defer gl.glDeleteVertexArrays(1, vao);
+    defer gl.glDeleteBuffers(1, vbo);
+
+    defer gl.glfwTerminate();
+
+    openglCheckError();
+
+    const startTime = gl.glfwGetTime();
     while (gl.glfwWindowShouldClose(window) == gl.GL_FALSE) {
-        //while (gl.glfwWindowShouldClose(window) == gl.GL_FALSE) {
-        //while (true) {
-        gl.glfwSwapBuffers(window);
+        gl.glClearColor(0.0, 0.0, 0.0, 0.0);
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT | gl.GL_STENCIL_BUFFER_BIT);
+
+        gl.glUseProgram(programId);
+
+        const timePassedSinceStart = gl.glfwGetTime() - startTime;
+        _ = timePassedSinceStart;
+        //std.debug.print("[INFO] Timed Passed: {d} '\n", .{gl.glfwGetTime() - startTime});
+
+        gl.glBindVertexArray(vao);
+        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 36);
+
+        gl.glfwSwapBuffers(window);
         gl.glfwPollEvents();
-        //std.debug.print("time: {}'\n", .{gl.glfwGetTime()});
     }
 }
+
+const vertices = [_]gl.GLfloat{ -0.5, -0.5, -0.5, 0.0, 0.0, 0.5, -0.5, -0.5, 1.0, 0.0, 0.5, 0.5, -0.5, 1.0, 1.0, 0.5, 0.5, -0.5, 1.0, 1.0, -0.5, 0.5, -0.5, 0.0, 1.0, -0.5, -0.5, -0.5, 0.0, 0.0, -0.5, -0.5, 0.5, 0.0, 0.0, 0.5, -0.5, 0.5, 1.0, 0.0, 0.5, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, 0.5, 1.0, 1.0, -0.5, 0.5, 0.5, 0.0, 1.0, -0.5, -0.5, 0.5, 0.0, 0.0, -0.5, 0.5, 0.5, 1.0, 0.0, -0.5, 0.5, -0.5, 1.0, 1.0, -0.5, -0.5, -0.5, 0.0, 1.0, -0.5, -0.5, -0.5, 0.0, 1.0, -0.5, -0.5, 0.5, 0.0, 0.0, -0.5, 0.5, 0.5, 1.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.0, 0.5, 0.5, -0.5, 1.0, 1.0, 0.5, -0.5, -0.5, 0.0, 1.0, 0.5, -0.5, -0.5, 0.0, 1.0, 0.5, -0.5, 0.5, 0.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.0, -0.5, -0.5, -0.5, 0.0, 1.0, 0.5, -0.5, -0.5, 1.0, 1.0, 0.5, -0.5, 0.5, 1.0, 0.0, 0.5, -0.5, 0.5, 1.0, 0.0, -0.5, -0.5, 0.5, 0.0, 0.0, -0.5, -0.5, -0.5, 0.0, 1.0, -0.5, 0.5, -0.5, 0.0, 1.0, 0.5, 0.5, -0.5, 1.0, 1.0, 0.5, 0.5, 0.5, 1.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.0, -0.5, 0.5, 0.5, 0.0, 0.0, -0.5, 0.5, -0.5, 0.0, 1.0 };
